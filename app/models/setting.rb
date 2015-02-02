e3caadb23af47022eac7cc45e0e934c76cedb4f9
Nowaker <nowaker@virtkick.io>
2015-02-01 19:46:38 -0600

    improve Setting.find_by_key

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,5 +1,6 @@
 class Setting < ActiveRecord::Base
   def self.find_by_key key, default = nil
-    Setting.where(key: key).first.try(:val)
+    val = Setting.where(key: key).first.try(:val)
+    val.nil? ? default : val
   end
 end
