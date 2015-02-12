1715018575316764eaa965f2676974bede89286e
m13 <m13@balloonstudio.pl>
2015-02-12 02:56:05 +0100

    add heatmap script

--- app/javascripts/appcommon.js
+++ app/javascripts/appcommon.js
@@ -1,6 +1,7 @@
 window.name = "NG_DEFER_BOOTSTRAP!";
 define(function(require) {
   require('jquery');
+  require('snippets/heatmap');
   require('snippets/analytics');
   require('jquery_ujs');
   require('bootstrap/collapse');
