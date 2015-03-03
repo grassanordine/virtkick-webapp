5da0ea5ea371745d1cc0716b032a6bfac9329296
Damian Kaczmarek <rush@rushbase.net>
2015-03-02 20:46:02 -0600

    Add mock request to home

--- spec/requests/application_controller_spec.rb
+++ spec/requests/application_controller_spec.rb
@@ -13,6 +13,16 @@ describe 'Demo sessions' do
     post guests_path
     expect(response).to redirect_to '/machines'
 
+    stub_request(:get, "http://0.0.0.0:8000/1/storages").
+        with(:headers => {:'Accept'=>'application/json'}).
+        to_return(
+            status: 200,
+            body: '{"errors": [], "response": {"secrets": [], "storages": [{"status": 1, "name": "virtkick-iso", "enabled": true, "volumes": 12, "type": "dir", "size": 264086360064}, {"status": 1, "name": "virtkick-hdd", "enabled": true, "volumes": 288, "type": "dir", "size": 264086360064}]}}',
+            headers: {
+              :'content-type' => 'application/json; charset=utf-8'
+            }
+        )
+
     get '/machines'
     expect(response).to render_template :home
 
